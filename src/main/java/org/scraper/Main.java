18724beea84b382ba0c2e69f94eec0b6a3ed3050
amineallali <amineallali9@gmail.com>
2024-07-31 12:46:34 +0100

    add code extraction method

--- src/main/java/org/scraper/Main.java
+++ src/main/java/org/scraper/Main.java
@@ -1,8 +1,6 @@
 package org.scraper;
 
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
+import org.openqa.selenium.*;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 import java.util.ArrayList;
@@ -17,9 +15,11 @@ public class Main {
         System.setProperty("webdriver.chrome.driver", "/Users/mac/Downloads/chromedriver-mac-x64/chromedriver");
 
         WebDriver driver = new ChromeDriver();
+        JavascriptExecutor js = (JavascriptExecutor) driver;
 
         try{
 
+            //sign up to gitHub
             driver.get("https://github.com/login");
 
             WebElement usernameField = driver.findElement(By.id("login_field"));
@@ -31,36 +31,121 @@ public class Main {
             WebElement loginButton = driver.findElement(By.name("commit"));
             loginButton.click();
 
-            Thread.sleep(15000);
+            Thread.sleep(19000);
 
+            //load java code pages
             driver.get("https://github.com/search?q=language%3AJava&type=code");
 
-            List<WebElement> codePages = new ArrayList<>();
             List<String> links = new ArrayList<>();
-            codePages = driver.findElements(By.cssSelector("td.blob-num a"));
 
+            //variable that store if there is a next page
+            boolean nextPage = true;
 
-            for(WebElement code: codePages ){
-                String fileUrl = code.getAttribute("href");
-                String regex = "https://github\\.com/[^#]+#L1";
+            while(nextPage){
 
-                Pattern pattern = Pattern.compile(regex);
-                Matcher matcher = pattern.matcher(fileUrl);
-                if (matcher.matches()) {
-                    links.add(fileUrl);
-                    System.out.println(fileUrl);
+                getUrls(driver, links);
+                //scroll down
+                js.executeScript("window.scrollTo(0, document.body.scrollHeight);");
+
+                //get the next page button
+                List<WebElement> nextButtons = driver.findElements(By.cssSelector("a[rel='next']"));
+
+                if (nextButtons.size() > 0){
+
+                    WebElement nextButton = nextButtons.getFirst();
+                    String isAriaDisabled = nextButton.getAttribute("aria-disabled");
+
+                    if (nextButton.isDisplayed() && ( isAriaDisabled == null || !isAriaDisabled.equals("true") ) ){
+                        //click the button if it's not disabled
+                        nextButton.click();
+                        //wait to the next page to load
+                        Thread.sleep(10000);
+                    }else {
+                        // update the nextPage variable if it's disabled
+                        nextPage = false;
+                    }
+                }else {
+                    nextPage = false;
                 }
-
             }
+
+            for(String url: links){
+                extractCode(driver,url);
+            }
+
+
+
 
         }catch(RuntimeException e){
             System.out.println("erreur"+e.getMessage());
         }catch (InterruptedException e) {
             throw new RuntimeException(e);
         }
-//        finally {
-//            driver.quit();
-//        }
 
     }
+
+
+
+    //function that extract urls of java code page
+    public static void getUrls(WebDriver driver,List<String> links ){
+
+        List<WebElement> codePages = driver.findElements(By.cssSelector("td.blob-num a"));
+
+        for(WebElement code: codePages ){
+            String fileUrl = code.getAttribute("href");
+            String regex = "https://github\\.com/[^#]+#L1";
+
+            Pattern pattern = Pattern.compile(regex);
+            Matcher matcher = pattern.matcher(fileUrl);
+            if (matcher.matches()) {
+                links.add(fileUrl);
+                System.out.println(fileUrl);
+            }
+
+        }
+
+    }
+
+
+    public static void extractCode(WebDriver driver, String url) {
+        try {
+            driver.get(url);
+            Thread.sleep(5000);
+
+            WebElement rawButton = driver.findElement(By.cssSelector("a[data-testid='raw-button']"));
+            rawButton.click();
+            Thread.sleep(5000);
+
+            String rawUrl = driver.getCurrentUrl();
+            driver.get(rawUrl);
+
+            // Extract the code from the <pre> tag
+            List<WebElement> preTags = driver.findElements(By.tagName("pre"));
+
+            if (preTags.isEmpty()) {
+                System.out.println("No <pre> tag found on the page: " + rawUrl);
+            } else {
+
+                WebElement preTag = preTags.getFirst();
+                String codeText = preTag.getText();
+
+                if (codeText.isEmpty()) {
+                    System.out.println("No code found in the <pre> tag at URL: " + rawUrl);
+                } else {
+                    System.out.println("Extracted Code:");
+                    System.out.println(codeText);
+                }
+            }
+
+        }catch (InterruptedException e) {
+            System.out.println("erreur"+e.getMessage());
+        }
+    }
+
+
+
+
+
+
+
 }
